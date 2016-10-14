d54417aedfe78eb8ed1e7a4658d2b7082578647b
Jim Quitevis <jquitevis@xmatters.com>
2016-10-13 22:33:33 -0700

    More fibonacci

--- src/main/java/com/lordbritishix/coursera/fibonacci/FibonacciPartialSum.java
+++ src/main/java/com/lordbritishix/coursera/fibonacci/FibonacciPartialSum.java
@@ -2,8 +2,14 @@ package com.lordbritishix.coursera.fibonacci;
 
 import java.util.*;
 
+/**
+ * This class computes the sum the fib numbers between fib(from) and fib(to) and then returns the last digit.
+ * This builds on a similar solution found in {@link FibonacciSumLastDigit}
+ */
 public class FibonacciPartialSum {
-    private static long getFibonacciPartialSumNaive(long from, long to) {
+    private static final int PISANO_LENGTH_FOR_MOD_10 = 60;
+
+    private long getFibonacciPartialSumNaive(long from, long to) {
         if (to <= 1) {
             return to;
         }
@@ -12,16 +18,16 @@ public class FibonacciPartialSum {
         long current = 1;
 
         for (long i = 0; i < from - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
+            long tmp_previous = previous % 10;
+            previous = current % 10;
             current = tmp_previous + current;
         }
 
         long sum = current;
 
         for (long i = 0; i < to - from; ++i) {
-            long tmp_previous = previous;
-            previous = current;
+            long tmp_previous = previous % 10;
+            previous = current % 10;
             current = tmp_previous + current;
             sum += current;
         }
@@ -29,11 +35,22 @@ public class FibonacciPartialSum {
         return sum % 10;
     }
 
+    long getFibonacciPartialSumFast(long from, long to) {
+        if (from > to) {
+            throw new IllegalArgumentException("to must be <= from");
+        }
+
+        return getFibonacciPartialSumNaive(from % PISANO_LENGTH_FOR_MOD_10, to % PISANO_LENGTH_FOR_MOD_10);
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+
+        FibonacciPartialSum fibonacciPartialSum = new FibonacciPartialSum();
+        System.out.println(fibonacciPartialSum.getFibonacciPartialSumFast(from, to));
     }
 }
 
--- src/main/java/com/lordbritishix/coursera/fibonacci/FibonacciSumLastDigit.java
+++ src/main/java/com/lordbritishix/coursera/fibonacci/FibonacciSumLastDigit.java
@@ -1,9 +1,20 @@
 package com.lordbritishix.coursera.fibonacci;
 
-import java.util.*;
+import java.util.Scanner;
 
+/**
+ * This class does the following:
+ * 1. Compute the sum of fib(n) from fib(0) to fib(n)
+ * 2. Get the last number of n
+ *
+ * Insight:
+ * This builds on the fact that the pisano of fib(n) % 10 is 60 - so we only need to calculate up to fib(60) to know
+ * the last number of the sum of any fib(n). Hence, any fib(n) == fib(n % 60)
+ */
 public class FibonacciSumLastDigit {
-    private static long getFibonacciSumNaive(long n) {
+    private static final int PISANO_LENGTH_FOR_MOD_10 = 60;
+
+    private long getFibonacciSumNaive(long n) {
         if (n <= 1) {
             return n;
         }
@@ -13,8 +24,8 @@ public class FibonacciSumLastDigit {
         long sum = 1;
 
         for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current;
+            long tmp_previous = previous % 10;
+            previous = current % 10;
             current = tmp_previous + current;
             sum += current;
         }
@@ -22,10 +33,22 @@ public class FibonacciSumLastDigit {
         return sum % 10;
     }
 
+    long getFibonacciSumFast(long n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return getFibonacciSumNaive(n % PISANO_LENGTH_FOR_MOD_10);
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumNaive(n);
+
+        FibonacciSumLastDigit fibonacciSumLastDigit = new FibonacciSumLastDigit();
+
+        long s = fibonacciSumLastDigit.getFibonacciSumFast(n);
         System.out.println(s);
     }
 }
